2dfade06a1a21802d3ece00d0dbb79f2d5bd0aa8
coherent17 <mnb51817@gmail.com>
2021-10-05 11:13:36 +0800

    fix readfile problem

--- HW1/hw1.c
+++ HW1/hw1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #define MAX_SIZE 10000
 
@@ -40,44 +41,59 @@ void printMatrix(matrix *A){
     printf("\n");
 }
 
+void printNonZeroTerm(matrix *A){
+    for (int i = 0; i < A->count;i++){
+        printf("value = %d, at column = %d, row = %d\n", A->term[i].value, A->term[i].column_index, A->term[i].row_index);
+    }
+}
+
 void printMatrixInfo(matrix *A){
     printf("Column of Matrix: %d\n", A->column);
     printf("Row of Matrix: %d\n", A->row);
     printf("Non Zero Term of Matrix: %d\n", A->count);
+    printf("The non-zero term of the matrix:\n");
+    printNonZeroTerm(A);
     printf("The content of the matrix:\n");
     printMatrix(A);
 }
 
-void matrixTranspose(matrix *A){
-    for (int i = 0; i < A->count;i++){
-        int temp = A->term[i].column_index;
-        A->term[i].column_index = A->term[i].row_index;
-        A->term[i].row_index = temp;
+matrix *matrixTranspose(matrix *A){
+    //allocate the mamory size to store the transpose matrix;
+    matrix *newMatrix = malloc(sizeof(matrix));
+    newMatrix->column = A->row;
+    newMatrix->row = A->column;
+    newMatrix->count = A->count;
+
+    //if A is nonzero matrix
+    if(A->count>0){
+        int newCount = 0;
+        for (int i = 1; i <= A->column;i++){
+            for (int j = 0; j < A->count;j++){
+                if(A->term[j].column_index==i){
+                    newMatrix->term[newCount].row_index = i;
+                    newMatrix->term[newCount].column_index = A->term[j].row_index;
+                    newMatrix->term[newCount].value = A->term[j].value;
+                    newCount++;
+                }
+            }
+        }
     }
-    int temp = A->column;
-    A->column = A->row;
-    A->row = temp;
+    return newMatrix;
 }
 
-int main(){
-    matrix *A = malloc(sizeof(matrix));
-    matrix *B = malloc(sizeof(matrix));
-    matrix **matrixptr = &A;
-
-    //initialize matrix A and B
-    initMatrix(&A);
-    initMatrix(&B);
-
+//read the input file as two matrix
+void readfile(matrix *A, matrix *B, matrix **matrixptr, char *filename){
     //temp array to store the last value of the column and row value
     //store[0] = column, store[1] = row
     int *store = malloc(sizeof(int) * 2);
 
-    //read all line into buffer
+    //read string into buffer
     char buffer[MAX_SIZE];
-    const char *filename = "sample.in";
+
     FILE *input = fopen(filename, "r");
+
     while(fscanf(input, "%s", buffer)!=EOF){
-        //printf("%s: ", buffer);
+        printf("%s: ", buffer);
         //meet "A"
         if(strcmp(buffer, "A")==0){
             fscanf(input, "%s", buffer); //read " ;"
@@ -113,13 +129,37 @@ int main(){
             (*matrixptr)->term[(*matrixptr)->count].row_index = (*matrixptr)->row;
             (*matrixptr)->count++;
         }
-        //printf("%d %d %d\n", (*matrixptr)->column, (*matrixptr)->row, (*matrixptr)->count);
+        printf("%d %d %d\n", (*matrixptr)->column, (*matrixptr)->row, (*matrixptr)->count);
     }
     B->column = store[0];
     B->row = store[1];
+}
+
+/*
+matrix *matrixMultiplication(matrix *A, matrix *B){
+    
+}
+*/
+
+int main(int argc, char *argv[]){
+    matrix *A = malloc(sizeof(matrix));
+    matrix *B = malloc(sizeof(matrix));
+    matrix **matrixptr = &A;
+
+    //initialize matrix A and B
+    initMatrix(&A);
+    initMatrix(&B);
+
+
+    char *filename = *(argv + 1);
+    char *outputName = *(argv + 2);
+    readfile(A, B, matrixptr, filename);
+
     printMatrixInfo(A);
     printMatrixInfo(B);
-    matrixTranspose(A);
-    printMatrixInfo(A);
+    matrix *Atranspose = matrixTranspose(A);
+    printMatrixInfo(Atranspose);
+    matrix *Btranspose = matrixTranspose(B);
+    printMatrixInfo(Btranspose);
     return 0;
 }
